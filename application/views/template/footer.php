bafd5ff00ec728478317dea1d512c72d2d84be5a
ahmadiupt <mi.dataahmadi@gmail.com>
2023-01-15 05:11:11 -0800

    Add Controller User Login

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -13,6 +13,6 @@
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
+        <script src="<?= base_url('assets/js/scripts.js')?>"></script>
     </body>
 </html>
